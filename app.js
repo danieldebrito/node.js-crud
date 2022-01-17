461459c75bce18ae5e65bd1d5c67bdfaeb46b7b3
Daniel de Brito <danieldebrito@outlook.com>
2022-01-17 18:38:35 -0300

    routes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var librosRouter = require('./routes/libros');
 
 var app = express();
@@ -25,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/libros', librosRouter);
 
 
